bd9e981d01f6b9befcde4d837ec5abfae43d1a4e
DESKTOP-BP2CG2T\Think <1326754391@qq.com>
2019-02-21 15:12:41 +0800

    新增pwa文件

---
+++ precache-manifest.dbe0ca9ab203c96ae45ee11160f9eb04.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ff69c8fca81469c28f589d5fad0616e",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "80962780a720a662aaa7",
+    "url": "/static/js/vendor.80962780.js"
+  },
+  {
+    "revision": "2c5d37a108202e131e2d841af6e51c63",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "dde7e2f1b16be11d77c5",
+    "url": "/static/js/vue.dde7e2f1.js"
+  },
+  {
+    "revision": "588079df8f1b8fde3b3cd691ee65bf06",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "7253881ecb893ceb68f6",
+    "url": "/static/js/manifest.7253881e.js"
+  },
+  {
+    "revision": "3742528143340e36915adf388d64060f",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "efbe0aa485b3aeaceb8ead9191f0c4c0",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "83409fb3fe88cb94d7d3df5fe97569c9",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "a8ba124764c3267e7383",
+    "url": "/static/js/index.a8ba1247.js"
+  },
+  {
+    "revision": "4dc6fdc67611efce36a3c500cbe1c0ec",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "b451e38c6ee4b03781b1019c518e7894",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "047f3021e153c19951f86dd209904243",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b8a42c4a3b00c4f75bc636f2923eaeca",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "a8ba124764c3267e7383",
+    "url": "/static/css/index.10774213.css"
+  },
+  {
+    "revision": "a08a96cfb2fcfe36f7f0e06030b169a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/favicon.ico"
+  }
+];
